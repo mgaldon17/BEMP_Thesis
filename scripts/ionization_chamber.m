c37f36fb7a30bd88f7d6830f9626bfc92f24a2c1
Manuel Galdon <galdon.manuel@outlook.com>
2022-10-18 09:29:00 +0200

    Fix geometry error, add IMP card, change M3 composition

--- scripts/ionization_chamber.m
+++ scripts/ionization_chamber.m
@@ -3,30 +3,31 @@
 FX = reshape(FX.',[],1);
 FY_156 = reshape(FY_156.',[],1);
 err_156 = reshape(err_156.',[],1);
-%FY_166 = reshape(FY_166.',[],1);
-%err_166 = reshape(err_166.',[],1);
+FY_166 = reshape(FY_166.',[],1);
+err_166 = reshape(err_166.',[],1);
 gray = true;
 errors = true;
 
 %Check if FX and FY and err lengths match
 [FY_1,err_1] = checkLengths(FX,FY_156, err_156);
-%[FY_2,err_2] = checkLengths(FX,FY_166, err_166);
+[FY_2,err_2] = checkLengths(FX,FY_166, err_166);
 
 %Convert MeV/g into Gy (1Gy=1J/Kg)
 convertIntoGray(FY_1, err_1, gray);
-%convertIntoGray(FY_2, err_2, gray);
+convertIntoGray(FY_2, err_2, gray);
 
 %Plot
 
 
-plot(FX, FY_1, 'r')
+plot(FX, FY_1, 'r') %F156
 hold on
-%plot(FX, FY_2, 'b')
-title('Linear plot of secondary electrons in water')
+plot(FX, FY_2, 'b') %F166
+hold on
+title('Linear plot of H')
 checkIfGray(gray)
 if errors == true
     errorbar(FX,FY_1,err_1.*FY_1, 'r', 'HandleVisibility','off')
-    %errorbar(FX,FY_2,err_2.*FY_2, 'b', 'HandleVisibility','off')
+    errorbar(FX,FY_2,err_2.*FY_2, 'b', 'HandleVisibility','off')
 end
 hold off
 %legend('F156', 'F166')
