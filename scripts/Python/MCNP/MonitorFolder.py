b0a8c133f9d8e138fb9ca197083615a3d413c835
Manuel Galdon <galdon.manuel@outlook.com>
2022-11-16 20:58:17 +0100

    Add Queue for thread info sharing and set objects

---
+++ scripts/Python/MCNP/MonitorFolder.py
@@ -0,0 +1,51 @@
+import os
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from MCNP import q
+
+
+class MonitorFolder(FileSystemEventHandler):
+    FILE_SIZE = 1000
+
+    def on_created(self, event):
+
+        print(event.src_path, event.event_type)
+
+        mctan = event.src_path.split("/")[-1]
+
+        if mctan[:2] == "mc":
+            q.put(mctan)
+        self.checkFolderSize(event.src_path)
+
+    def on_modified(self, event):
+        # print(event.src_path, event.event_type)
+        self.checkFolderSize(event.src_path)
+
+    def checkFolderSize(self, src_path):
+        if os.path.isdir(src_path):
+            if os.path.getsize(src_path) > self.FILE_SIZE:
+                pass
+        else:
+            if os.path.getsize(src_path) > self.FILE_SIZE:
+                pass
+
+
+if __name__ == "__main__":
+    src_path = "output/"
+
+    event_handler = MonitorFolder()
+    observer = Observer()
+    observer.schedule(event_handler, path=src_path, recursive=True)
+    print("Monitoring started")
+
+    observer.start()
+
+    try:
+        while (True):
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
--- scripts/Python/MCNP/main.py
+++ scripts/Python/MCNP/main.py
@@ -1,7 +1,16 @@
-
-from runMCNP import runMCNP
+import logging
+from threading import Thread
+from MonitorFolder import MonitorFolder
+from MCNP import MCNP
+from watchdog.observers import Observer
 
 if __name__ == '__main__':
-    runMCNP()
 
-
+    OUTPUT_PATH = "output/"
+    gray = False
+    event_handler = MonitorFolder()
+    observer = Observer()
+    observer.schedule(event_handler, path=OUTPUT_PATH, recursive=True)
+    logging.info("Monitoring started")
+    watcher = Thread(observer.start())
+    run = Thread(MCNP.runMCNP())
