8b8b073c6c0b630ead3aa61a04cccb0ae8611d18
Manuel Galdon <galdon.manuel@outlook.com>
2024-05-18 12:41:03 +0200

    Redefine project structure and optimize methods for the simulation of the corrosion layer

---
+++ Python/MCNPSimulationScripts/main.py
@@ -0,0 +1,8 @@
+from Python.MCNPSimulationScripts.withCorrosion.simulate_material_fraction import run
+
+if __name__ == '__main__':
+    run(["MEDAPP_Source.txt"], ["/hydromagnesite/materials_0%_water + 100%_hydro.txt",
+                                "/hydromagnesite/materials_99%_hydro + 1%_water.txt",
+                                "/hydromagnesite/materials_98%_hydro + 2%_water.txt",
+                                "/hydromagnesite/materials_97%_hydro + 3%_water.txt"
+                                ], "hydromagnesite", "10E8", True, False)
---
+++ Python/MCNPSimulationScripts/utilities/manageOutput.py
@@ -0,0 +1,28 @@
+import os
+import logging
+
+def clean_output_folder():
+    if os.path.exists("output/"):
+        print('Folder exists!')
+        os.chdir("output/")
+        all_files = os.listdir()
+
+        for f in all_files:
+            os.remove(f)
+
+        logging.info("All files in output_nps10E7 folder were removed")
+    else:
+        print('Folder does not exist.')
+
+def create_output_folder(OUTPUT_PATH):
+    # Create output dir if not exist
+    isExist = os.path.exists(OUTPUT_PATH)
+    if not isExist:
+        # Create a new directory because it does not exist
+        print(os.getcwd())
+        os.makedirs(OUTPUT_PATH)
+        print("The new directory is created!")
+
+if __name__ == '__main__':
+    clean_output_folder()
+    create_output_folder("your_output_path_here")
