85fadc6cceba3a9683e960be0358206603db483b
Manuel Galdon <galdon.manuel@outlook.com>
2024-05-20 12:05:06 +0200

    Encode auth_params

--- Python/MCNPSimulationScripts/simulation/notification_bot/message_center.py
+++ Python/MCNPSimulationScripts/simulation/notification_bot/message_center.py
@@ -3,20 +3,24 @@ import time
 
 import pytz
 import tweepy
-
+import base64
 
 def read_parameters():
-    input_files_dir = os.path.join(os.path.dirname(__file__), '..', 'inputFilesParts')
+    input_files_dir = os.path.join(os.path.dirname(__file__), 'config', '')
 
-    with open(os.path.join(input_files_dir, 'message_center.txt')) as file:
-        parameters = {key.strip(): value.strip() for line in file if '=' in line for key, value in
-                      (line.strip().split('='),)}
-        return parameters
+    with open(os.path.join(input_files_dir, 'auth_params.txt'), 'rb') as file:
+        base64_encoded_parameters = file.read()
+
+    # Decode the base64-encoded parameters
+    decoded_parameters = base64.b64decode(base64_encoded_parameters).decode()
+
+    parameters = {key.strip(): value.strip() for line in decoded_parameters.split('\n') if '=' in line for key, value in [line.strip().split('=')]}
+    return parameters
 
 
 class MessageCenter:
 
-    # In order to be able to read the auth parameters, those must be written in a txt file in config/message_center.txt
+    # In order to be able to read the auth parameters, those must be written in a txt file in config/auth_params.txt
     # CONSUMER_KEY = ...
     # CONSUMER_SECRET = ...
     # ACCESS_TOKEN = ...
